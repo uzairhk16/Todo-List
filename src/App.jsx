8b14a15a96c9094b547925ff3de3a2dd250a5e8f
Uzair Khan <uzairhk16@gmail.com>
2024-07-27 16:57:07 +0530

    Add new files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LudoBoard from './LudoBoard'
+import TodoList from './TodoList'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <TodoList />
+    </>
+  )
+}
+
+export default App
---
+++ src/TodoList.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./TodoList.css";
+
+export default function TodoList() {
+
+    let [todos, setTodos] = useState(["Code"]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        setTodos([...todos, newTodo]);
+        setNewTodo("");
+    }
+
+    let updateNewTodo = (event) => {
+        setNewTodo(event.target.value);
+    }
+    return (
+        <div>
+            <h2>Priotize your task's</h2>
+            <div className="todolist">
+                <div className="inpp">
+                    <input className="inputclass"
+                        placeholder="          add new task"
+                        value={newTodo}
+                        onChange={updateNewTodo} />
+                    <button className="buttonclass" onClick={addNewTask}>Add</button>
+                </div>
+                <hr />
+                <h3>Today's List</h3>
+                <ul>
+                    {
+                        todos.map((todo) => (
+                            <li>{todo}</li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+}
