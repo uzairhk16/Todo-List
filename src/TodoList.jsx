2b58fbe0dbc3935c76f531d27ea4e311ea3563d8
Uzair Khan <uzairhk16@gmail.com>
2024-08-05 16:05:37 +0530

    add new files

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
 import "./TodoList.css";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList() {
 
-    let [todos, setTodos] = useState(["Codegit "]);
+    let [todos, setTodos] = useState([{ task: "sample", id: uuidv4() }]);
     let [newTodo, setNewTodo] = useState("");
 
     let addNewTask = () => {
-        setTodos([...todos, newTodo]);
+        setTodos((prevTodos) => {
+            return [...todos, { task: newTodo, id: uuidv4() }]
+        });
         setNewTodo("");
     }
 
     let updateNewTodo = (event) => {
         setNewTodo(event.target.value);
     }
+
+    let deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id!= id));
+    }
+
     return (
         <div>
             <h2>Priotize your task's</h2>
@@ -28,13 +36,14 @@ export default function TodoList() {
                 <hr />
                 <h3>Today's List</h3>
                 <ul>
-                    {
-                        todos.map((todo) => (
-                            <li>{todo}</li>
-                        ))
-                    }
+                    {todos.map((todo) => (
+                        <li key={todo.id}>
+                            <span> {todo.task} </span>
+                            <button onClick={() => deleteTodo(todo.id)}>delete</button>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
-    )
+    );
 }
